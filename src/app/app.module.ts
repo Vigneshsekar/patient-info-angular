b379d91f46237d1b71391f9e24362990ae8da271
Vignesh S <svignesh0308@gmail.com>
2017-09-11 09:40:15 +0530

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule, MdToolbarModule, MdIconModule, MdTabsModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { PatientInfoComponent } from './patient-info/patient-info.component';
+import { TopToolbarComponent } from './common/top-toolbar/top-toolbar.component';
+import { NavTabsComponent } from './common/nav-tabs/nav-tabs.component';
+import { AddPatientComponent } from './add-patient/add-patient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientInfoComponent,
+    TopToolbarComponent,
+    NavTabsComponent,
+    AddPatientComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule, 
+    MdCheckboxModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
