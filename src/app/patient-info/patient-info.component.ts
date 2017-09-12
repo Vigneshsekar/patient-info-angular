feb9edfe7b70a4081a6720f31c3d182b750290d7
Vignesh S <svignesh0308@gmail.com>
2017-09-12 23:55:26 +0530

    Display patient info

--- src/app/patient-info/patient-info.component.ts
+++ src/app/patient-info/patient-info.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { PatientDataService } from '../patient-data.service';
+import { Patient } from '../patient';
+import { FirebaseListObservable } from 'angularfire2/database';
+import {DataSource} from '@angular/cdk/collections';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import { Subject } from 'rxjs/Subject'
 
 @Component({
   selector: 'app-patient-info',
@@ -7,9 +14,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatientInfoComponent implements OnInit {
 
-  constructor() { }
+  public patients: FirebaseListObservable<Patient[]>;
+
+  searchRecords;
+  startAt = new Subject()
+  endAt = new Subject()
+  lastKeypress: number = 0;
+  startSearch: boolean = false;
+
+  constructor(private patientDataService: PatientDataService) {
+   }
 
   ngOnInit() {
+    this.patients = this.patientDataService.getAllPatientsList()
+
+    this.patientDataService.getPatients(this.startAt, this.endAt)
+    .subscribe(searchRecords => this.searchRecords = searchRecords)
   }
 
+  search($event) {
+    ($event.target.value.length > 1) ? this.startSearch = true : this.startSearch = false;
+    if ($event.timeStamp - this.lastKeypress > 200) {
+      let q = $event.target.value
+      this.startAt.next(q)
+      this.endAt.next(q+"\uf8ff")
+    }
+    this.lastKeypress = $event.timeStamp
+  }
 }
